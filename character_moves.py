33bc9ccc22ef0222c8665859710dc21d91b8b703
folder <ht51566@gmail.com>
2025-09-19 21:56:17 +0900

    전체 흐름 기본 검증

--- character_moves.py
+++ character_moves.py
@@ -2,7 +2,18 @@ from pico2d import *
 
 open_canvas()
 
+
+def move_rectangle():
+    print("Move Rectangle")
+    pass
+
+def move_circle():
+    print("Move Circle")
+    pass
+
 while True:
+    move_rectangle()
+    move_circle()
     pass
 
 close_canvas()
